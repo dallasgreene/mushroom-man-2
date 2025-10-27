e650d226d625c0ff89a43de40cb2eacd0a25b6d8
Adam Freedman <dama33@gmail.com>
2025-10-26 20:41:21 -0400

    made more agnostic

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -3,14 +3,10 @@ extends Node3D
 
 var rotate_speed = .2
 var end_basis: Basis
-var rotating = false
-var tween: Tween
-	
-func _exit_tree() -> void:
-	tween.finished.disconnect(_on_rotation_finish)
+var moving = false
 		
 func _physics_process(_delta: float) -> void:
-	if !rotating:
+	if !moving:
 		if Input.is_action_just_pressed("move_forward"):
 			attempt_move(Vector2(0, 1))
 		elif Input.is_action_just_pressed("move_backward"):
@@ -21,12 +17,12 @@ func _physics_process(_delta: float) -> void:
 			attempt_move(Vector2(-1, 0))
 		elif Input.is_action_just_pressed("rotate_right"):
 			end_basis = Basis(transform.basis.rotated(Vector3(0,1,0),-PI/2))
-			rotating = true
-			create_tween().tween_method(rotate_player,transform.basis,end_basis,rotate_speed).finished.connect(_on_rotation_finish)
+			moving = true
+			create_tween().tween_method(rotate_player,transform.basis,end_basis,rotate_speed).finished.connect(_on_moving_finish)
 		elif Input.is_action_just_pressed("rotate_left"):
 			end_basis = Basis(transform.basis.rotated(Vector3(0,1,0),PI/2))
-			rotating = true
-			create_tween().tween_method(rotate_player,transform.basis,end_basis,rotate_speed).finished.connect(_on_rotation_finish)
+			moving = true
+			create_tween().tween_method(rotate_player,transform.basis,end_basis,rotate_speed).finished.connect(_on_moving_finish)
 		if Input.is_action_just_pressed("melee_attack"):
 			pass
 
@@ -40,8 +36,8 @@ func attempt_move(direction: Vector2):
 func rotate_player(value: Basis):
 	transform.basis = value
 
-func _on_rotation_finish():
-	rotating = false
+func _on_moving_finish():
+	moving = false
 	rotation_degrees.y = roundf(rotation_degrees.y)
 	print(rotation_degrees)
 	
