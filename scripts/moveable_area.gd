b241925abcd9dc560b01e16a724f69a8edd412fe
Dallas <dgreene1641@gmail.com>
2025-10-26 20:27:38 -0400

    get moveable check working

--- scripts/moveable_area.gd
+++ scripts/moveable_area.gd
@@ -1,5 +1,15 @@
-class_name MoveableArea extends Node3D
+class_name MoveableArea extends CSGBox3D
+
+
+@export var the_shape: CollisionShape3D
+@export var the_area: Plane
+
+
+func _exit_tree():
+	print("Node is leaving the scene tree.")
+	# Perform cleanup or other actions here
 
 
 func _ready() -> void:
-	pass
+	assert(rotation == Vector3.ZERO, "DO NOT ROTATE MoveableAreas!!")
+	Moveable.register_moveable_area(self)
