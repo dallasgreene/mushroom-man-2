d40608e6049adbfcb49ba1ea96bba1306d43b891
Dallas Greene <dgreene1641@gmail.com>
2025-11-04 23:38:29 -0500

    rename stuff

---
+++ globals/enemy_action.gd
@@ -0,0 +1,36 @@
+extends Node
+
+
+var enemy_movement_signal_count: int = 0
+var enemy_attack_signal_count: int = 0
+
+var enemy_movement_signal_count_limit: int = 0
+var enemy_attack_signal_count_limit: int = 0
+
+func _ready():
+	SignalBus.enemy_movement_received.connect(_enemy_movement_counter)
+	SignalBus.enemy_attack_received.connect(_enemy_attack_counter)
+
+## Triggers all enemies to start taking their turn. Movement happens first,
+## then attacks
+func enemies_take_turn() -> void:
+	enemy_movement_signal_count_limit = SignalBus.enemy_movement_start.get_connections().size()
+	enemy_attack_signal_count_limit = SignalBus.enemy_attack_start.get_connections().size()
+	if SignalBus.enemy_movement_received.get_connections().size()>0:
+		SignalBus.enemy_movement_start.emit()
+	elif SignalBus.enemy_attack_received.get_connections().size()>0:
+		SignalBus.enemy_attack_start.emit()
+
+func _enemy_movement_counter():
+	enemy_movement_signal_count += 1
+	if enemy_movement_signal_count >= enemy_movement_signal_count_limit:
+		enemy_movement_signal_count_limit = 0
+		enemy_movement_signal_count = 0
+		SignalBus.enemy_attack_start.emit()
+
+func _enemy_attack_counter():
+	enemy_attack_signal_count += 1
+	if enemy_attack_signal_count >= enemy_attack_signal_count_limit:
+		enemy_attack_signal_count_limit = 0
+		enemy_attack_signal_count = 0
+		SignalBus.enemies_finished_acting.emit()
--- globals/global.gd
+++ globals/global.gd
@@ -9,15 +9,7 @@ const TILE_SIZE: int = 2
 
 var player: Player
 var next_room_id: int = 0
-var enemy_movement_signal_count: int = 0
-var enemy_attack_signal_count: int = 0
 
-var enemy_movement_signal_count_limit: int = 0
-var enemy_attack_signal_count_limit: int = 0
-
-func _ready():
-	SignalBus.enemy_movement_received.connect(_enemy_movement_counter)
-	SignalBus.enemy_attack_received.connect(_enemy_attack_counter)
 
 func register_player(player_input:Player):
 	self.player = player_input
@@ -27,25 +19,3 @@ func register_player(player_input:Player):
 func get_next_available_room_id() -> int:
 	next_room_id += 1
 	return next_room_id - 1
-
-func timeline_process() -> void:
-	enemy_movement_signal_count_limit = SignalBus.enemy_movement_start.get_connections().size()
-	enemy_attack_signal_count_limit = SignalBus.enemy_attack_start.get_connections().size()
-	if SignalBus.enemy_movement_received.get_connections().size()>0:
-		SignalBus.enemy_movement_start.emit()
-	elif SignalBus.enemy_attack_received.get_connections().size()>0:
-		SignalBus.enemy_movement_start.emit()
-
-func _enemy_movement_counter():
-	enemy_movement_signal_count += 1
-	if enemy_movement_signal_count >= enemy_movement_signal_count_limit:
-		enemy_movement_signal_count_limit = 0
-		enemy_movement_signal_count = 0
-		SignalBus.enemy_attack_start.emit()
-
-func _enemy_attack_counter():
-	enemy_attack_signal_count += 1
-	if enemy_attack_signal_count >= enemy_attack_signal_count_limit:
-		enemy_attack_signal_count_limit = 0
-		enemy_attack_signal_count = 0
-		SignalBus.turn_ended.emit()
