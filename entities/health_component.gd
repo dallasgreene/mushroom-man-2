50e2e11c5aa5f48f5d302c111b5f51f60bfb47b7
Dallas Greene <dgreene1641@gmail.com>
2025-12-01 08:19:18 -0500

    add visible health bars

--- entities/health_component.gd
+++ entities/health_component.gd
@@ -1,16 +1,29 @@
 extends Node3D
 class_name HealthComponent
 
-@export_range(1,100) var max_health: int
+@export_range(1, 100) var max_health: int
+@export var display_healthbar: bool = true
 var current_health: int
+
+var health_display_bar: ProgressBar
 
 signal died
 
 func _ready() -> void:
 	current_health = max_health
-func take_damage(damage:int):
+	if display_healthbar:
+		health_display_bar = %ProgressBar
+		health_display_bar.max_value = max_health
+		health_display_bar.value = current_health
+	else:
+		$Sprite3D.queue_free()
+		$SubViewport.queue_free()
+
+
+func take_damage(damage: int):
 	print("Health was ", current_health)
 	current_health -= damage
 	print("Current Health is ", current_health)
+	health_display_bar.value = max(0, current_health)
 	if current_health <= 0:
 		died.emit()
